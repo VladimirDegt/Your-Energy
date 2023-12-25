2e4bb13359e03556acab84d9cb40761887d27b52
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-12-25 13:19:42 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,33 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
+  if (command === 'serve') {
+    return {
+      define: {
+        global: {},
       },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+      root: 'src',
+      build: {
+        rollupOptions: {
+          input: glob.sync('./src/*.html'),
+        },
+        outDir: '../dist',
+      },
+      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+  } else {
+    return {
+      define: {
+        _global: {},
+      },
+      root: 'src',
+      build: {
+        rollupOptions: {
+          input: glob.sync('./src/*.html'),
+        },
+        outDir: '../dist',
+      },
+      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+  }
 });
